93cef7bc34c01e1509b9c6e523aecdd98284353b
Odenyi <brianodenyi@gmail.com>
2023-05-06 13:44:35 +0300

    quiz game oop final

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,4 +1,22 @@
 class QuizBrain:
-    def __init__(self,question_list):
+    def __init__(self, question_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = question_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text}  ('True' /'False )")
+        self.check_answer(user_answer,current_question.answer)
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    def check_answer(self,answer,correct_answer):
+        if answer.lower() != correct_answer.lower():
+            print("Thats wrong")
+        else:
+            print("you got the correct answer ")
+            self.score +=1
+        print(f"The correct answer was :{correct_answer}\n and your score is {self.score}/{self.question_number}")
